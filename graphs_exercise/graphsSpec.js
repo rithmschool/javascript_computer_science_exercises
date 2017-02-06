21682ac4596aac049bed2405c2d123407b29a093
Elie Schoppik <eschoppik@gmail.com>
2017-02-05 19:35:19 -0800

    adding first version of graphs

---
+++ graphs_exercise/graphsSpec.js
@@ -0,0 +1,78 @@
+var expect = chai.expect
+var graph;
+
+beforeEach(function(){
+  graph = new Graph
+})
+
+describe("#addVertex", function(){
+  it("", function(){
+
+  });
+  it("", function(){
+
+  });
+});
+
+describe("#addEdge", function(){
+  it("", function(){
+
+  });
+  it("", function(){
+
+  });
+});
+
+describe("#removeEdge", function(){
+  it("", function(){
+
+  });
+  it("", function(){
+
+  });
+});
+
+describe("#removeVertex", function(){
+  it("", function(){
+
+  });
+  it("", function(){
+
+  });
+});
+
+describe("#depthFirstSearch", function(){
+  it("", function(){
+
+  });
+  it("", function(){
+
+  });
+});
+
+describe("#breadthFirstSearch", function(){
+  it("", function(){
+
+  });
+  it("", function(){
+
+  });
+});
+
+describe("#djikstra", function(){
+  it("", function(){
+
+  });
+  it("", function(){
+
+  });
+});
+
+describe("#aStar", function(){
+  it("", function(){
+
+  });
+  it("", function(){
+
+  });
+});
