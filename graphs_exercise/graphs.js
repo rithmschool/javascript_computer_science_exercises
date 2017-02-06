7487e6a1cab1560622efc4827a6b1abd0109ad95
Elie Schoppik <eschoppik@gmail.com>
2017-02-06 15:34:39 -0800

    adding graph solutions

--- graphs_exercise/graphs.js
+++ graphs_exercise/graphs.js
@@ -1,3 +1,139 @@
 function Graph(){
-
+    this.verticies = [];
+    this.adjacencyList = {};
 }
+
+Graph.prototype.addVertex = function(vertex) {
+  this.verticies.push(vertex);
+
+  this.adjacencyList[vertex] = [];
+};
+
+Graph.prototype.addEdge = function(vertex1, vertex2) {
+  this.adjacencyList[vertex1].push(vertex2);
+  this.adjacencyList[vertex2].push(vertex1);
+};
+
+Graph.prototype.removeVertex = function(vertex) {
+  var index = this.verticies.indexOf(vertex);
+  if(index > -1) this.verticies.splice(index, 1);
+
+  while(this.adjacencyList[vertex].length) {
+    var adjacentVertex = this.adjacencyList[vertex].pop();
+    this.removeEdge(adjacentVertex, vertex);
+  }
+};
+
+Graph.prototype.removeEdge = function(vertex1, vertex2) {
+  var index1 = this.adjacencyList[vertex1] ? this.adjacencyList[vertex1].indexOf(vertex2) : -1;
+  var index2 = this.adjacencyList[vertex2] ? this.adjacencyList[vertex2].indexOf(vertex1) : -1;
+  if(index1 > - 1) this.adjacencyList[vertex1].splice(index1, 1);
+  if(index2 > - 1) this.adjacencyList[vertex2].splice(index2, 1);
+};
+
+Graph.prototype.breadthFirstSearch = function(start){
+    // Create an empty queue
+    var queue = [start];
+    var result = [];
+    var visited = {};
+    var currentVertex;
+    //Set the starting vertex to visited
+    visited[start] = true;
+
+    //While there is still remaining vertexes in queue
+    while (currentVertex = queue.shift()) {
+       result.push(currentVertex);
+
+       for(var i = 0; i < this.adjacencyList[currentVertex].length; i++){
+         var val = this.adjacencyList[currentVertex][i]
+          if(!visited[val]){
+            visited[val] = true
+            queue.push(this.adjacencyList[currentVertex][i])
+          }
+       }
+    }
+    return result
+}
+
+Graph.prototype.depthFirstSearch = function(start){
+    // Create an empty stack
+    var stack = [start]
+    var result = [];
+    var visited = {};
+    var currentVertex;
+
+    //Set the starting vertex to visited
+    visited[start] = true;
+
+    //While there is still remaining vertexes in stack
+    while (stack.length) {
+       currentVertex = stack.pop();
+       result.push(currentVertex);
+
+       for(var i = 0; i < this.adjacencyList[currentVertex].length; i++){
+         var val = this.adjacencyList[currentVertex][i]
+          if(!visited[val]){
+            visited[val] = true
+            stack.push(this.adjacencyList[currentVertex][i])
+          }
+       }
+    }
+    return result
+}
+
+function WeightedGraph(){
+    Graph.apply(this,arguments)
+}
+
+WeightedGraph.prototype = Object.create(Graph.prototype)
+
+WeightedGraph.prototype.addEdge = function(vertex1, vertex2, weight) {
+  this.adjacencyList[vertex1].push({node:vertex2, weight});
+  this.adjacencyList[vertex2].push({node:vertex1, weight});
+};
+
+WeightedGraph.prototype.Dijkstra = function(nodeA, nodeB) {
+  var dist = {}, unvisited = {}, min, tempDist, current, previous = {};
+
+  for (let node in this.adjacencyList) {
+    unvisited[node] = true;
+    previous[node] = undefined;
+    node === nodeA ? dist[node] = 0 : dist[node] = Infinity;
+  }
+
+  while(unvisited[nodeB]) {
+    min = Infinity;
+    current = Object.keys(unvisited).reduce(function(acc, node) {
+      if (dist[node] < min) {
+        acc = node;
+        min = dist[node];
+      }
+      return acc;
+    }, undefined);
+
+    delete unvisited[current];
+
+    for (let val of this.adjacencyList[current]) {
+      if (unvisited[val.node]) {
+        tempDist = dist[current] + this.adjacencyList[current][this.adjacencyList[current].indexOf(val)].weight
+        if (tempDist < dist[val.node]) {
+          previous[val.node] = current
+          dist[val.node] = tempDist;
+        }
+      }
+    }
+  }
+
+  function displayPath(previousPaths, node){
+    var paths = [node];
+    while(previousPaths[node]){
+      paths.unshift(previousPaths[node])
+      node = previousPaths[node]
+    }
+    return paths;
+  }
+
+  var path = displayPath(previous, nodeB)
+
+  return [dist[nodeB], path];
+};
--- graphs_exercise/graphsSpec.js
+++ graphs_exercise/graphsSpec.js
@@ -6,73 +6,204 @@ beforeEach(function(){
 })
 
 describe("#addVertex", function(){
-  it("", function(){
-
+  it("should add a vertex to the verticies array", function(){
+    graph.addVertex('A')
+    graph.addVertex('B')
+    graph.addVertex('C')
+    expect(graph.verticies[0]).to.equal('A')
+    expect(graph.verticies[1]).to.equal('B')
+    expect(graph.verticies[2]).to.equal('C')
+    expect(graph.verticies.length).to.equal(3)
   });
-  it("", function(){
-
+  it("should add a key in the adjacency list with a value as an empty array", function(){
+    graph.addVertex('A')
+    graph.addVertex('B')
+    graph.addVertex('C')
+    expect(graph.adjacencyList['A']).to.deep.equal([])
+    expect(graph.adjacencyList['B']).to.deep.equal([])
+    expect(graph.adjacencyList['C']).to.deep.equal([])
   });
 });
 
 describe("#addEdge", function(){
-  it("", function(){
-
-  });
-  it("", function(){
-
+  it("should add the appropriate edges to the adjacency list", function(){
+    graph.addVertex('A')
+    graph.addVertex('B')
+    graph.addVertex('C')
+    graph.addVertex('D')
+    graph.addEdge('A','B')
+    graph.addEdge('A','C')
+    graph.addEdge('B','D')
+    graph.addEdge('C','D')
+    expect(graph.verticies.length).to.equal(4)
+    expect(graph.adjacencyList['A']).to.contain('B','C')
+    expect(graph.adjacencyList['B']).to.contain('A','D')
+    expect(graph.adjacencyList['C']).to.contain('A','D')
+    expect(graph.adjacencyList['D']).to.contain('C','B')
   });
 });
 
 describe("#removeEdge", function(){
-  it("", function(){
+  it("should remove the verticies from the adjacency list", function(){
+    graph.addVertex('A')
+    graph.addVertex('B')
+    graph.addVertex('C')
+    graph.addVertex('D')
+    graph.addEdge('A','B')
+    graph.addEdge('A','C')
+    graph.addEdge('B','D')
+    graph.addEdge('C','D')
 
-  });
-  it("", function(){
-
+    graph.removeEdge('B','A')
+    graph.removeEdge('C','D')
+    expect(graph.verticies.length).to.equal(4)
+    expect(graph.adjacencyList['A']).to.contain('C')
+    expect(graph.adjacencyList['B']).to.contain('D')
+    expect(graph.adjacencyList['C']).to.contain('A')
+    expect(graph.adjacencyList['D']).to.contain('B')
   });
 });
 
 describe("#removeVertex", function(){
-  it("", function(){
+  it("should remove the vertex as well as any edges", function(){
+    graph.addVertex('A')
+    graph.addVertex('B')
+    graph.addVertex('C')
+    graph.addVertex('D')
+    graph.addEdge('A','B')
+    graph.addEdge('A','C')
+    graph.addEdge('B','D')
+    graph.addEdge('C','D')
 
-  });
-  it("", function(){
-
+    graph.removeVertex('C')
+    graph.removeVertex('B')
+    expect(graph.verticies.length).to.equal(2)
+    expect(graph.adjacencyList['A']).to.be.empty
+    expect(graph.adjacencyList['D']).to.be.empty
   });
 });
 
 describe("#depthFirstSearch", function(){
-  it("", function(){
+  it("should return an array of the nodes searched", function(){
+    graph.addVertex('S')
+    graph.addVertex('P')
+    graph.addVertex('U')
+    graph.addVertex('X')
+    graph.addVertex('Q')
+    graph.addVertex('Y')
+    graph.addVertex('V')
+    graph.addVertex('R')
+    graph.addVertex('W')
+    graph.addVertex('T')
 
-  });
-  it("", function(){
+    graph.addEdge('S','P')
+    graph.addEdge('S','U')
 
+    graph.addEdge('P','X')
+    graph.addEdge('U','X')
+
+    graph.addEdge('P','Q')
+    graph.addEdge('U','V')
+
+    graph.addEdge('X','Q')
+    graph.addEdge('X','Y')
+    graph.addEdge('X','V')
+
+    graph.addEdge('Q','R')
+    graph.addEdge('Y','R')
+
+    graph.addEdge('Y','W')
+    graph.addEdge('V','W')
+
+    graph.addEdge('R','T')
+    graph.addEdge('W','T')
+
+    expect(graph.depthFirstSearch('S')).to.deep.equal(["S", "U", "V", "W", "T", "R", "Q", "Y", "X", "P"])
   });
 });
 
 describe("#breadthFirstSearch", function(){
-  it("", function(){
+  it("should return an array of the nodes searched", function(){
+    graph.addVertex('S')
+    graph.addVertex('P')
+    graph.addVertex('U')
+    graph.addVertex('X')
+    graph.addVertex('Q')
+    graph.addVertex('Y')
+    graph.addVertex('V')
+    graph.addVertex('R')
+    graph.addVertex('W')
+    graph.addVertex('T')
 
-  });
-  it("", function(){
+    graph.addEdge('S','P')
+    graph.addEdge('S','U')
 
+    graph.addEdge('P','X')
+    graph.addEdge('U','X')
+
+    graph.addEdge('P','Q')
+    graph.addEdge('U','V')
+
+    graph.addEdge('X','Q')
+    graph.addEdge('X','Y')
+    graph.addEdge('X','V')
+
+    graph.addEdge('Q','R')
+    graph.addEdge('Y','R')
+
+    graph.addEdge('Y','W')
+    graph.addEdge('V','W')
+
+    graph.addEdge('R','T')
+    graph.addEdge('W','T')
+
+    expect(graph.breadthFirstSearch('S')).to.deep.equal(["S", "P", "U", "X", "Q", "V", "Y", "R", "W", "T"])
   });
 });
 
-describe("#djikstra", function(){
-  it("", function(){
+describe("#dijkstra", function(){
+  it("should return an array of the total distance and the shortest path", function(){
+    var g = new WeightedGraph
+    g.addVertex('S')
+    g.addVertex('P')
+    g.addVertex('U')
+    g.addVertex('X')
+    g.addVertex('Q')
+    g.addVertex('Y')
+    g.addVertex('V')
+    g.addVertex('R')
+    g.addVertex('W')
+    g.addVertex('T')
 
-  });
-  it("", function(){
+    g.addEdge('S','P', 2)
+    g.addEdge('S','U', 3)
 
+    g.addEdge('P','X', 4)
+    g.addEdge('U','X', 1)
+
+    g.addEdge('P','Q', 5)
+    g.addEdge('U','V', 3)
+
+    g.addEdge('X','Q', 7)
+    g.addEdge('X','Y', 6)
+    g.addEdge('X','V', 8)
+
+    g.addEdge('Q','R', 2)
+    g.addEdge('Y','R', 1)
+
+    g.addEdge('Y','W', 3)
+    g.addEdge('V','W', 4)
+
+    g.addEdge('R','T', 6)
+    g.addEdge('W','T', 7)
+
+    expect(g.Dijkstra('S','T')[0]).to.equal(15)
+    expect(g.Dijkstra('S','T')[1]).to.deep.equal(['S','P','Q','R','T'])
   });
 });
 
 describe("#aStar", function(){
-  it("", function(){
-
-  });
-  it("", function(){
+  xit("", function(){
 
   });
 });
