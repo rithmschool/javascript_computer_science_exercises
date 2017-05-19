e5c88dc96c918348ae77a7dc8a6dff17c60025db
Albert Kam <albertkam91@gmail.com>
2017-05-18 22:24:37 -0700

    completed insert and find and inorder traversal

--- binary_search_trees_exercise/binarySearchTree.js
+++ binary_search_trees_exercise/binarySearchTree.js
@@ -31,3 +31,76 @@ BinarySearchTree.prototype.insertIteratively = function(val){
 	}
 	return this;
 }
+
+BinarySearchTree.prototype.insertRecursively = function(val, node=this.root){
+	if(this.root === null){
+		this.root = new Node(val);
+		return this;
+	}
+	if(val > node.value){
+		if(node.right === null){
+			node.right = new Node(val);
+			return this;
+		} else {
+			return this.insertRecursively(val, node.right);
+		}
+	} else {
+		if(node.left === null){
+			node.left = new Node(val);
+			return this;
+		} else {
+			return this.insertRecursively(val, node.left);
+		}
+	}
+}
+
+BinarySearchTree.prototype.findIteratively = function(value){
+	var curr = this.root
+	while(curr !== null){
+		if(value === curr.value){
+			return curr
+		} else if(value > curr.value){
+			curr = curr.right
+		} else {
+			curr = curr.left
+		}
+
+	}
+	return undefined
+}
+
+BinarySearchTree.prototype.findRecursively = function(value, node=this.root){
+	if(this.root.value === value) return this.root
+	if(node === null) return undefined
+	if(node.value === value) return node
+	if(value > node.value){
+		return this.findRecursively(value,node.right)
+	} else {
+		return this.findRecursively(value,node.left)
+	}
+}
+
+BinarySearchTree.prototype.toArray = function(){
+	var arr = [];
+
+	function helper(node){
+		if(node !== null){
+			helper(node.left);
+			arr.push(node.value);
+			helper(node.right);
+		}
+	}
+	helper(this.root);
+	return arr;
+}
+
+
+
+
+
+
+
+
+
+
+
