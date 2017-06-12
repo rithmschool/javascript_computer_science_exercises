8622ef1528c09e95727517f1843a6a99328f60c7
Tim Garcia <tigarcia@users.noreply.github.com>
2017-06-12 07:46:11 -0700

    Fixed bubble sort bug

--- sorting_algorithms_exercise/sorting.js
+++ sorting_algorithms_exercise/sorting.js
@@ -4,7 +4,7 @@ function swap(arr, idx1, idx2) {
 
 function bubbleSort(arr){
 
-  for (let i = arr.length; i > 0; i--) {
+  for (let i = arr.length - 1; i > 0; i--) {
     for (let j = 0; j < i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         swap(arr, j, j + 1);
